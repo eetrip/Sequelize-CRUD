81a38289fc0f441ef23eceba93af69e935fe4ee1
eetrip <eeshantripathi111@gmail.com>
2020-02-21 15:41:13 +0530

    prettier applied

--- models/item.js
+++ models/item.js
@@ -1,9 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
-	var Item = sequelize.define('Item', {
-		name:  DataTypes.STRING,
-		price: DataTypes.DECIMAL,
-		category:  DataTypes.STRING
-	});
+  var Item = sequelize.define("Item", {
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    category: DataTypes.STRING
+  });
 
-	return Item;
-}
+  return Item;
+};
--- server.js
+++ server.js
@@ -1,28 +1,23 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 3000;
-var db = require('./models');
-var apiRoutes = require('./app/routes/apiRoutes.js');
-
+var db = require("./models");
+var apiRoutes = require("./app/routes/apiRoutes.js");
 
 //data parsing part
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vmd.api+json" }));
 
 //static directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-apiRoutes(app,db);
+apiRoutes(app, db);
 
-db.sequelize.sync().then(function(){
-
-app.listen(3000, function() {
-	console.log(`Listening on port ${PORT}`);
-	});
+db.sequelize.sync().then(function() {
+  app.listen(3000, function() {
+    console.log(`Listening on port ${PORT}`);
+  });
 });
-
-
-
